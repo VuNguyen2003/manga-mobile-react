3e119c20ea3c813299c3fb96429a60dcea37eabf
Vũ Nguyễn <116508377+VuNguyen2003@users.noreply.github.com>
2023-12-14 18:10:22 +0700

    test mb app

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, ScrollView, TextInput } from 'react-native';
+
+
+function MyButton(){
+  return(
+    <View>
+      <Button
+        title="Right button"
+        onPress={() => Alert.alert('Right button pressed')}
+      />
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello react</Text>
       <StatusBar style="auto" />
+      <MyButton />
     </View>
   );
 }
